62d38be348b2700bbfaf9b5577461d41f83d74cb
Mohamed <moh.i.ahmed@hotmail.com>
2024-04-29 16:00:18 +0100

    3

--- main.tf
+++ main.tf
@@ -217,8 +217,8 @@ resource "aws_route_table" "private_route_table" {
 #Create two route table associations, one for the public subnet and one for the private subnet.
 #public subnet will be associated with the public route table
 resource "aws_route_table_association" "public_rt_assoc" {
-  count          = 1
-  subnet_id      = aws_subnet.public_subnet[count.0].id
+  count          = 2
+  subnet_id      = aws_subnet.public_subnet[count.index].id
   route_table_id = aws_route_table.public_route_table.id
 }
 
